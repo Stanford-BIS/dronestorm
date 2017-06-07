062d8a521db5bf93379b9b467e83246670d57dea
Arnav Gudibande <arnav.gudibande@gmail.com>
2017-06-07 18:22:29 +0000

    Added Adafruit library & servo control

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import smbus
+from __future__ import division
+import smbus, Adafruit_PCA9685, time
+
+pwm = Adafruit_PCA9685.PCA9685()
+
+servo_min = 150
+servo_max = 600
+
+pwm.set_pwm_freq(60)
+
+channel = input("Enter channel of servo: ")
+
+cont = True
+
+while cont:
+    pwm.set_pwm(channel, 0, servo_min)
+    time.sleep(1)
+    pwm.set_pwm(channel, 0, servo_max)
+    time.sleep(1)
