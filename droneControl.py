71685ad7cf2551bdad8f8da6097b1a4ba1fcbd13
Arnav Gudibande <arnav.gudibande@gmail.com>
2017-06-22 19:10:07 +0000

    Updated DroneControl library

--- droneControl.py
+++ droneControl.py
@@ -1,70 +1,136 @@
 from __future__ import division
+from pyMultiwii import MultiWii
 import Adafruit_PCA9685, time
 
-class DroneController:
+"""
+Library to acquire Telemetry Data from Naze32 board (angx, angy and heading).
+Furthermore, contains methods to send Roll/Pitch/Yaw signals to the Naze32
+flight controller from a Raspberry pi via a Feather board using PWM signals.
+"""
 
-    # channel 1 -> Yaw
-    # channel 2 -> Pitch
-    # channel 3 -> Roll
+class DroneControl:
 
-    # range of values: 1.1ms -> 1.9ms
+    # Range of Values (Pulse Width): 1.1ms -> 1.9ms
+    MIN_WIDTH = 1.10
+    MAX_WIDTH = 1.90
+    MED_WIDTH = 1.50
 
-    pwm = Adafruit_PCA9685.PCA9685()
+    # Featherboard channels
+    YAW_CHANNEL = 1
+    PITCH_CHANNEL = 2
+    ROLL_CHANNEL = 3
 
-    pwm.set_pwm_freq(1/.023)    # ~45.45 Hz
+    SCALING_FACTOR = 1.4 / 1.5
 
-    # used to adjust pulse width
+    def __init__(self):
+        '''
+        Initializes PWM object
+        '''
+
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(1/.023)    # ~45.45 Hz
+
     def set_servo_pulse(channel, pulse):
+        '''
+        Used to set a certain Pulse Width on a channel
+        '''
         pulse_length = 1000000    # 1,000,000 us per second
         pulse_length //= 1/.022       # ~45.45 Hz
         pulse_length //= 4096     # 12 bits of resolution
         pulse *= 1000
         pulse //= pulse_length
-        pwm.set_pwm(channel, 0, int(pulse))
 
-    # used to reset channels
-    def reset():
-        for i in range(6):
-            set_servo_pulse(i, 1.4)
-
-    SCALING_FACTOR = 1.4 / 1.5
-    MIN_WIDTH = 1.10
-    MAX_WIDTH = 1.90
+        self.pwm.set_pwm(channel, 0, int(pulse))
 
     def convertWidth(width):
-        return width*SCALING_FACTOR
+        '''
+        Internal conversion method to send PWM signals to Adafruit library
+        '''
+        return width * SCALING_FACTOR
 
     def setYaw(width):
-        set_servo_pulse(1, convertWidth(width))
+        '''
+        Sets the Yaw to a desired PWM width
+        '''
+        width_c, valid = isValid(width)
+        set_servo_pulse(YAW_CHANNEL, width_correx)
+
+        if not valid:
+            print("WARNING: Yaw out of range!")
 
     def setPitch(width):
-        set_servo_pulse(2, convertWidth(width))
+        '''
+        Sets the Pitch to a desired PWM width
+        '''
+        width_c, valid = isValid(width)
+        set_servo_pulse(PITCH_CHANNEL, width_correx)
+
+        if not valid:
+            print("WARNING: Pitch out of range!")
 
     def setRoll(width):
-        set_servo_pulse(3, convertWidth(width))
+        '''
+        Sets the Roll to a desired PWM width
+        '''
+        width_c, valid = isValid(width)
+        set_servo_pulse(ROLL_CHANNEL, width_correx)
 
-    def demoApp():
+        if not valid:
+            print("WARNING: Roll out of range!")
 
+    def isValid(width):
+        '''
+        Verifies that the PWM signals are in the accepted range.
+        If not, the MAX_WIDTH or MIN_WIDTH is returned
+        '''
+        width_c = convertWidth(width)
+
+        if(width_c > MAX_WIDTH):
+            return convertWidth(MAX_WIDTH), False
+        elif(width_c < MIN_WIDTH):
+            return convertWidth(MIN_WIDTH), False
+        else:
+            return width_c, True
+
+    def reset():
+        '''
+        Resets channels 0-6 on the feather board; used as cleanup measure
+        '''
+        for i in range(6):
+            set_servo_pulse(i, MED_WIDTH)
+
+    def getData(board, arg):
+        '''
+        Returns the Attitude telemetry data from the Naze32 flight controller
+
+        Parameters: Board MultiWii Object, Desired data point {angx, angy, heading}
+        Returns: double
+        '''
+        board.getData(MultiWii.ATTITUDE)
+
+        if arg == 'angx' or arg == 'angy' or arg == 'heading':
+            return board.attitude[arg]
+        else:
+            print("Invalid argument\n")
+            board.closeSerial()
+
+    def signalControlExample():
+        '''
+        Sets the Roll/Pitch/Yaw on the Naze32 flight controller
+        to maximum then minimum pulse widths
+        '''
         reset()
-
         time.sleep(1)
 
-        setYaw(1.9)
-        setPitch(1.9)
-        setRoll(1.9)
+        setYaw(MAX_WIDTH)
+        setPitch(MAX_WIDTH)
+        setRoll(MAX_WIDTH)
 
         time.sleep(2)
 
-        setYaw(1.1)
-        setPitch(1.1)
-        setRoll(1.1)
+        setYaw(MIN_WIDTH)
+        setPitch(MIN_WIDTH)
+        setRoll(MIN_WIDTH)
 
         time.sleep(2)
-
-        setYaw(1.5)
-        setPitch(1.5)
-        setRoll(1.5)
-
-# RC Multiplexer
-
-demoApp()
+        reset()
