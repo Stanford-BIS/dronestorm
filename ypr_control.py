16268b97a50a69d95ac810ae5e9a5592979adc14
jacisbell <jacisbell@gmail.com>
2017-07-12 18:36:58 -0600

    Update ypr_control.py

--- ypr_control.py
+++ ypr_control.py
@@ -1,72 +1,90 @@
-from DroneControl import DroneComm
+from DroneControl import DroneComm, PID
 import sys
 import time
 import redis
 import numpy as np
-import math
 
 # Flight stabilization app using the DroneControl Library
 # Current roll and pitch values are acquired from the flight controller
 # then pulse width signals are determined to fix the error
 
-drone = DroneComm(port=None)
-dt = 0.01
+drone = DroneComm()
 
+r = redis.StrictRedis()
+r.set('yaw0', drone.get_yaw())
+r.set('z0', 0.0)
+r.set('y0', 0.0)
+
+# Zigler Nichols estimated tuning parameters
+Ku_yaw = 0.2
+Tu_yaw = 0.4
 # Proportion coefficients: how strongly the error should be corrected
-
-Kp_yaw  = 0.35
-Kd_yaw  = 0.2
+Kp_yaw  = Ku_yaw * 0.45
+Kd_yaw  = 0.2*Kp_yaw * Tu_yaw * .125
 Ki_yaw  = 0.
 
-out_limit_yaw = 1.0
+out_limit = 1.0
 
 error_yaw = 0
 int_error_yaw = 0
-int_error_limit_yaw = out_limit_yaw
+int_error_limit = out_limit
 d_error_yaw = 0
-d_error_prev_yaw = np.zeros(3)
-d_error_idx = 0
-d_error_yaw_filt = 0.9
-error_yaw_prev = 0
 
-
-#roll variables
-Kp_roll  = 0.35
-Kd_roll  = 0.2
+#roll parameters
+Ku_roll = 0.2
+Tu_roll = 0.4
+# Proportion coefficients: how strongly the error should be corrected
+Kp_roll  = Ku_roll * 0.45
+Kd_roll  = 0.2*Kp_roll * Tu_roll * .125
 Ki_roll  = 0.
 
-out_limit_roll = 1.0
+out_limit = 1.0
 
 error_roll = 0
 int_error_roll = 0
-int_error_limit_roll = out_limit_roll
+int_error_limit = out_limit
 d_error_roll = 0
-d_error_prev_roll = np.zeros(3)
-d_error_roll_filt = 0.9
-error_roll_prev = 0
 
-#Pitch variables
-Kp_pitch  = 0.35
-Kd_pitch = 0.2
-Ki_pitch = 0.
+#pitch parameters
+Ku_pitch = 0.2
+Tu_pitch = 0.4
+# Proportion coefficients: how strongly the error should be corrected
+Kp_pitch  = Ku_pitch * 0.45
+Kd_pitch  = 0.2*Kp_pitch * Tu_pitch * .125
+Ki_pitch  = 0.
 
-out_limit_pitch = 1.0
+out_limit = 1.0
 
-error_pitch= 0
-int_error_pitch= 0
-int_error_limit_pitch = out_limit_pitch
-d_error_pitch= 0
-d_error_prev_pitch = np.zeros(3)
-d_error_pitch_filt = 0.9
-error_pitch_prev = 0
+error_pitch = 0
+int_error_pitch = 0
+int_error_limit = out_limit
+d_error_pitch = 0
 
-# Start program by placing drone on a flat surface to ensure accurate
-# calibration Values
+############### Start program by placing drone on a flat surface to ensure accurate
 
-r = redis.StrictRedis()
-r.set('yaw0', 0.0)
-r.set('y0', 0.0)
-r.set('x0', 0.0)
+def center_yaw_error(error_yaw):
+    if error_yaw > 180:
+        error_yaw -= 360
+    elif error_yaw < -180:
+        error_yaw += 360
+    return error_yaw
+
+yaw_controller = PID(
+    Kp_yaw, Kd_yaw, Ki_yaw,
+    lambda : yaw0, drone.get_yaw, drone.get_dyaw,
+    center_yaw_error, out_limit)
+
+roll_controller = PID(
+    Kp_roll, Kd_roll, Ki_roll,
+    lambda : z0, r.get_z, drone.get_roll,    #state is z coordinate from camera, derivative is approx the roll value
+    lambda x: x, out_limit)                     #####CHECK used lambda x: x for the center_error function (and for pitch)
+
+pitch_controller = PID(
+    Kp_pitch, Kd_pitch, Ki_pitch,
+    lambda : y0, r.get_y, drone.get_pitch,   #state is y coordinate from camera, derivative is approx the pitch value
+    lambda x: x, out_limit)
+
+###############3 run the control
 
 while r.get('angle') is None:
     time.sleep(1.0)
@@ -74,98 +92,37 @@ while r.get('angle') is None:
 
 try:
     while (True):
-        yaw0 = float(r.get('yaw0'))
-        yaw = -float(r.get('yaw'))
-    # Error between desired and actual roll/pitch
-#       yaw = yaw + dyaw * dt  #add the imu derivative data to interpolate between frames
-        error_yaw_prev = error_yaw
-        error_yaw = yaw0 - yaw
-
-        d_error_prev_yaw[d_error_idx] = (error_yaw - error_yaw_prev) / dt
-        d_error_idx += 1
-        d_error_idx %= 3
-        d_error_yaw = np.mean(d_error_prev_yaw)
-
-        int_error_yaw += error_yaw
-        int_error_yaw = np.clip(int_error_yaw,
-            -int_error_limit_yaw, int_error_limit_yaw)
-
-        output_yaw = np.clip(
-            Kp_yaw * error_yaw + Ki_yaw * int_error_yaw + Kd_yaw * d_error_yaw,
-            -out_limit_yaw, out_limit_yaw)
-
+        # calibration Values
+        yaw0 = drone.get_yaw()
+        z0=r.get('z0')
+        y0=r.get('y0')
+        
+        #step controllers
+        output_yaw = yaw_controller.step()
         sys.stdout.write(
-            "yaw0:%7.2f yaw:%5.1f output_yaw:%6.3f\r"%
-            (yaw0, yaw, output_yaw))
+            "yaw0:%5.1f yaw:%5.1f dyaw:%5.0f output_yaw:%6.3f\r"%
+            (yaw_controller.ref, yaw_controller.state, yaw_controller.dstate,
+             output_yaw))
+
+        output_roll = roll_controller.step()
+        sys.stdout.write(
+            "roll0:%5.1f roll:%5.1f droll:%5.0f output_roll:%6.3f\r"%
+            (roll_controller.ref, roll_controller.state, roll_controller.dstate,
+             output_roll))
+
+        output_pitch = pitch_controller.step()
+        sys.stdout.write(
+            "pitch0:%5.1f pitch:%5.1f dpitch:%5.0f output_pitch:%6.3f\r"%
+            (pitch_controller.ref, pitch_controller.state, pitch_controller.dstate,
+             output_pitch))
+
         sys.stdout.flush()
 
         # Set corrective rates
-        drone.set_yaw_rate(-output_yaw)#use - when apriltag is on the bottom
-
-        #############roll
-        roll0 = float(r.get('y0'))
-        y = -float(r.get('y'))
-        x = -float(r.get('x'))
-        #change axes to drone axis
-        roll = -x*math.sin(yaw)+y*math.cos(yaw)
-
-        # Error between desired and actual roll/pitch
-#       roll = roll + droll * dt  #add the imu derivative data to interpolate between frames
-        error_roll_prev = error_roll
-        error_roll = roll0 - roll
-
-        d_error_prev_roll[d_error_idx] = (error_roll - error_roll_prev) / dt
-        d_error_roll = np.mean(d_error_prev_roll)
-
-        int_error_roll += error_roll
-        int_error_roll = np.clip(int_error_roll,
-            -int_error_limit_roll, int_error_limit_roll)
-
-        output_roll = np.clip(
-            Kp_roll * error_roll + Ki_roll * int_error_roll + Kd_roll * d_error_roll,
-            -out_limit_roll, out_limit_roll)
-
-        sys.stdout.write(
-            "roll0:%7.2f roll:%5.1f output_roll:%6.3f\r"%
-            (roll0, roll, output_roll))
-        sys.stdout.flush()
-
-        # Set corrective rates
-        drone.set_roll_rate(output_roll)
-
-        #############pitch
-        pitch0 = float(r.get('x0'))
- 
-        #change axes to drone axis
-        pitch = x*math.cos(yaw)+y*math.sin(yaw)
-
-        # Error between desired and actual pitch/pitch
-#       pitch = pitch + dpitch * dt  #add the imu derivative data to interpolate between frames
-        error_pitch_prev = error_pitch
-        error_pitch = pitch0 - pitch
-
-        d_error_prev_pitch[d_error_idx] = (error_pitch - error_pitch_prev) / dt
-        d_error_pitch = np.mean(d_error_prev_pitch)
-
-        int_error_pitch += error_pitch
-        int_error_pitch = np.clip(int_error_pitch,
-            -int_error_limit_pitch, int_error_limit_pitch)
-
-   output_pitch = np.clip(
-            Kp_pitch * error_pitch + Ki_pitch * int_error_pitch + Kd_pitch * d_error_pitch,
-            -out_limit_pitch, out_limit_pitch)
-
-        sys.stdout.write(
-            "pitch0:%7.2f pitch:%5.1f output_pitch:%6.3f\r"%
-            (pitch0, pitch, output_pitch))
-        sys.stdout.flush()
-
-        # Set corrective rates
+        drone.set_yaw_rate(output_yaw)
+        drone.set_roll_rate(output_roll)        ########CHECK correct way of setting roll and pitch??
         drone.set_pitch_rate(output_pitch)
 
 except (KeyboardInterrupt, SystemExit):
     # Graceful Exit
     drone.exit()
-
-
-
