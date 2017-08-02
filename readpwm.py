250038eb4e935ebf324919d30856c72b55ffb80a
arnav.gudibande <arnav.gudibande@gmail.com>
2017-08-02 18:00:16 +0000

    Implemented redis

--- readpwm.py
+++ readpwm.py
@@ -1,14 +1,17 @@
-import time, sys
+import time, sys, redis
 import numpy as np
 import RPi.GPIO as GPIO
-from DroneControl import comm
 
-FREQ = 45.45
-T = 0.022
-PW = 0
-D = PW/T * 100
+YAW_CHN = 27
+ROLL_CHN = 22
+PITCH_CHN = 17
+AUX1_CHN = 26
+THR_CHN = 13
 
 GPIO.setmode(GPIO.BCM)
+
+host = '127.0.0.1'
+r = redis.StrictRedis(host=host)
 
 def measurePWM(channel):
     GPIO.setup(channel, GPIO.IN)
@@ -34,12 +37,6 @@ def measurePWM(channel):
 
     return np.mean(dt)
 
-YAW_CHN = 27
-ROLL_CHN = 22
-PITCH_CHN = 17
-AUX1_CHN = 26
-THR_CHN = 13
-
 try:
     while(True):
         yaw = measurePWM(YAW_CHN)
@@ -47,6 +44,12 @@ try:
         roll = measurePWM(ROLL_CHN)
         thr = measurePWM(THR_CHN)
         aux1 = measurePWM(AUX1_CHN)
+
+        r.set('roll', roll)
+        r.set('pitch', pitch)
+        r.set('yaw', yaw)
+        r.set('thr', thr)
+        r.set('aux1', aux1)
 
         sys.stdout.write(
             "roll:%.5f pitch:%.5f yaw:%.5f thr:%.5f aux1:%.5f\r"%
