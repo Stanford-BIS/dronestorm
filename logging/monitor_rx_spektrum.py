66c5d9302cd78ade8c025362d09a68fa4c06c821
Sam Fok <samfok@stanford.edu>
2017-09-15 11:55:58 -0700

    fix benchmark and package __init__

--- logging/monitor_rx_spektrum.py
+++ logging/monitor_rx_spektrum.py
@@ -8,7 +8,7 @@ rrx = SpektrumRemoteReceiver()
 try:
     rrx.align_serial()
     while True:
-        rc_data = rrx.get_data()
+        rc_data = rrx.read_data()
         sys.stdout.write(
             "    %4d     %4d     %4d     %4d     %4d     %4d\r"%tuple(
             rc_data[:6]))
