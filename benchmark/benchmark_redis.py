b247d11c112db4537fd2cf8473299bc5d977e2dc
Sam Fok <sambfok@gmail.com>
2017-09-23 15:11:12 -0700

    fix redis benchmark

--- benchmark/benchmark_redis.py
+++ benchmark/benchmark_redis.py
@@ -1,9 +1,11 @@
 """Benchmark the redis_util"""
 from __future__ import print_function
 import time
-from dronestorm import redis_util as r
+import numpy as np
+from dronestorm import redis_util
 
-rdb = r.DBRedis()
+N = 100
+rdb = redis_util.DBRedis()
 
 # reset
 start = time.time()
@@ -12,36 +14,52 @@ reset_dt = time.time() - start
 print("Resetting the redis database took %.3fms"%(reset_dt*1000))
 
 # set IMU data using set calls
-start = time.time()
-rdb.set(r.REDIS_IMU_DROLL, 1000)
-rdb.set(r.REDIS_IMU_DPITCH, 1000)
-rdb.set(r.REDIS_IMU_DYAW, 1000)
-rdb.set(r.REDIS_IMU_DDX, 1000)
-rdb.set(r.REDIS_IMU_DDY, 1000)
-rdb.set(r.REDIS_IMU_DDZ, 1000)
-set_dt = time.time() - start
-print("setting imu data took %.3fms"%(set_dt*1000))
+set_dt = np.zeros(N)
+for i in range(N):
+    start = time.time()
+    rdb.rdb.set(redis_util.REDIS_IMU_DROLL, 1000)
+    rdb.rdb.set(redis_util.REDIS_IMU_DPITCH, 1000)
+    rdb.rdb.set(redis_util.REDIS_IMU_DYAW, 1000)
+    rdb.rdb.set(redis_util.REDIS_IMU_DDX, 1000)
+    rdb.rdb.set(redis_util.REDIS_IMU_DDY, 1000)
+    rdb.rdb.set(redis_util.REDIS_IMU_DDZ, 1000)
+    set_dt[i] = time.time() - start
+set_dt *= 1000 # to ms
+print("setting imu data time mean:%.3fms median:%.3fms std:%.3fms"%(
+    np.mean(set_dt), np.median(set_dt), np.std(set_dt)))
 
 # get IMU data using get calls
-start = time.time()
-imu_droll = rdb.get_int(r.REDIS_IMU_DROLL)
-imu_dpitch = rdb.get_int(r.REDIS_IMU_DPITCH)
-imu_dyaw = rdb.get_int(r.REDIS_IMU_DYAW)
-imu_ddx = rdb.get_int(r.REDIS_IMU_DDX)
-imu_ddy = rdb.get_int(r.REDIS_IMU_DDY)
-imu_ddz = rdb.get_int(r.REDIS_IMU_DDZ)
-get_dt = time.time() - start
-print("getting imu data took %.3fms"%(get_dt*1000))
+get_dt = np.zeros(N)
+for i in range(N):
+    start = time.time()
+    imu_droll = rdb.rdb.get(redis_util.REDIS_IMU_DROLL)
+    imu_dpitch = rdb.rdb.get(redis_util.REDIS_IMU_DPITCH)
+    imu_dyaw = rdb.rdb.get(redis_util.REDIS_IMU_DYAW)
+    imu_ddx = rdb.rdb.get(redis_util.REDIS_IMU_DDX)
+    imu_ddy = rdb.rdb.get(redis_util.REDIS_IMU_DDY)
+    imu_ddz = rdb.rdb.get(redis_util.REDIS_IMU_DDZ)
+    get_dt[i] = time.time() - start
+get_dt *= 1000 # to ms
+print("getting imu data time mean:%.3fms median:%.3fms std:%.3fms"%(
+    np.mean(get_dt), np.median(get_dt), np.std(get_dt)))
 
 # set IMU data using set into pipeline calls
-start = time.time()
-rdb.set_imu([100, 100, 100, 100, 100, 100])
-pipelined_set_dt = time.time() - start
-print("pipelined setting imu data took %.3fms"%(pipelined_set_dt*1000))
+pipelined_set_dt = np.zeros(N)
+for i in range(N):
+    start = time.time()
+    redis_util.set_imu(rdb, [100, 100, 100, 100, 100, 100])
+    pipelined_set_dt[i] = time.time() - start
+pipelined_set_dt *= 1000 # to ms
+print("pipelined setting imu data time mean:%.3fms median:%.3fms std:%.3fms"%(
+    np.mean(pipelined_set_dt), np.median(pipelined_set_dt), np.std(pipelined_set_dt)))
 
 # get IMU data using set/get into pipeline calls
-start = time.time()
-imu_droll, imu_dpitch, imu_dyaw, imu_ddx, imu_ddy, imu_ddz = (
-    rdb.get_imu())
-pipelined_get_dt = time.time() - start
-print("pipelined getting imu data took %.3fms"%(pipelined_get_dt*1000))
+pipelined_get_dt = np.zeros(N)
+for i in range(N):
+    start = time.time()
+    imu_droll, imu_dpitch, imu_dyaw, imu_ddx, imu_ddy, imu_ddz = (
+        redis_util.get_imu(rdb))
+    pipelined_get_dt[i] = time.time() - start
+pipelined_get_dt *= 1000 # to ms
+print("pipelined getting imu data time mean:%.3fms median:%.3fms std:%.3fms"%(
+    np.mean(pipelined_get_dt), np.median(pipelined_get_dt), np.std(pipelined_get_dt)))
