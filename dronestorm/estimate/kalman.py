1d6964293968dff571be9ee61413523d47d56c0b
John Mern <jmern91@stanford.edu>
2017-10-04 13:21:03 -0700

    debugged EKF

--- dronestorm/estimate/kalman.py
+++ dronestorm/estimate/kalman.py
@@ -1,103 +1,71 @@
-"""Defined Kalman filters for state estimation"""
 import numpy as np 
 from state_matrix import * 
 from sensors import *
 
 class EKF: 
-    """Extended Kalman filter
-    
+	def __init__(self, R):
+		self.mu = np.zeros([12,1])
+		self.sigma = np.eye(12)*0.01
+		self.R = R
+		self.sensor_idx = {0:GPS, 1:AGL, 2:IMU}
+		self.Q_idx = {0: np.eye(6)*0.5,
+					  1: np.eye(1)*0.01**2,
+					  2: np.eye(9)*0.05**2
+			}
 
-    dmu/dt = 
+	def initialize(self, Zgps):
+		self.Z0 = {0:Zgps, 1:None, 2:None}
 
-    Parameters
-    ----------
-    R: 
-        noise covariance matrix. estimate of white noise
+	def predict(self, U, dt):
+		mud = x_dot(self.mu, U)
+		mu_ = self.mu + mud*dt
+		A, _, _, _ = state_matrix(self.mu, U, x_dot)
+		sigma_ = (A.dot(self.sigma)).dot(A.T) + self.R
+		return mu_, sigma_
 
-    Attributes
-    ----------
-    mu: 12 entry numpy column vector
-        current state estimate
-        [x, y, z, roll, pitch, yaw, dx, dy, dz, w_roll, w_pitch, w_yaw]
-    sigma:
-        current belief covariance 
-    R
-        plant noise covariance
-    Q:
-        sensor noise covariance
-    Q_idx
-    """
-    def __init__(self, R):
-        self.mu = np.zeros([12, 1])
-        self.sigma = np.eye(12)*0.01
-        self.R = R
-        self.sensor_idx = {0:GPS, 1:AGL, 2:IMU}
-        self.Q_idx = {0: np.eye(6)*4.5/2,
-                      1: np.eye(1)*0.01,
-                      2: np.eye(6)*0.05,}
-    
-    def initialize(self, Zgps, Zimu, Zagl):
-        self.Z0 = {0:Zgps, 1:None, 2:None}
-        # self.Zimu0 = Zimu
-        # self.Zagl0 = Zagl
-    
-    def predict(self, U, dt):
-        mud = x_dot(self.mu, U) # dmu/dt
-        mu_ = self.mu + mud*dt
-        # linarize around current mu for predicted sigma
-        A, _, _, _ = state_matrix(self.mu, U, mud)
-        sigma_ = (A.dot(self.sigma)).dot(A.T) + self.R
-        return mu_, sigma_
-    
-    def update(self, mu_, sigma_, U, sensors=None, measurements=None):
-        """Correction step"""
-        if sensors is not None: 
-            for i, sensor in enumerate(sensors): 
-                z_i = self.sensor_idx[i](mu_, U, self.Z0[i])
-                H_i = gen_H(self.sensor_idx[i], mu_, U, self.Z0[i])
-                zi = measurements[i]
-    
-                if 'z_' in local():
-                    z_ = np.vstack([z_, z_i])
-                else:
-                    z_ = z_i
-    
-                if 'z' in local():
-                    z = np.vstack([z, zi])
-                else:
-                    z = zi
-                
-                if 'H' in local():
-                    H = np.vstack([H, H_i])
-                else:
-                    H = H_i
-    
-                if 'Q' in local():
-                    m, n = Q.shape
-                    Q_ = self.Q_idx[i]
-                    m_, n_ = Q_.shape
-                    Q = np.vstack([Q, np.zeros([m_,n])])
-                    Q__ = np.vstack([np.zeros([m, n_]), Q_])
-                    Q = np.hstack([Q, Q__])
-                else:
-                    Q = self.Q_idx[i]
-                y = z - z_  # predicted - measured
-                S = H.dot(sigma_).dot(H.T) + Q 
-                K = sigma_.dot(H.T).dot(np.linalg.inv(S)) # optimal Kalman gain
-                self.mu = mu_ + K.dot(y)
-                self.sigma = (np.eye(12) - K.dot(H)).dot(sigma_)
-        else:
-            self.mu = mu_
-            self.sigma = sigma_
-    
-    def step(self, U, dt, sensors=None, measurements=None):
-        """Step the kalman filter
+	def update(self, mu_, sigma_, U, sensors=None, measurements=None):
+		if sensors is not None: 
+			for i, sensor in enumerate(sensors): 
+				z_i = self.sensor_idx[sensor](mu_, U, self.Z0[sensor])
+				H_i = gen_H(self.sensor_idx[sensor], mu_, U, self.Z0[sensor])
+				zi = measurements[i]
 
-        Parameters
-        ----------
-        U: prior motor command
-        dt: time step
-        """
-        mu_, sigma_ = self.predict(U, dt)
-        self.update(mu_, sigma_, sensors, measurements)
-        return self.mu, self.sigma 
+				if 'z_' in locals():
+					z_ = np.vstack([z_.reshape([-1,1]), z_i.reshape([-1,1])])
+				else:
+					z_ = z_i
+
+				if 'z' in locals():
+					z = np.vstack([z.reshape([-1,1]), zi.reshape([-1,1])])
+				else:
+					z = zi
+				
+				if 'H' in locals():
+					H = np.vstack([H, H_i])
+				else:
+					H = H_i
+
+				if 'Q' in locals():
+					m, n = Q.shape
+					Q_ = self.Q_idx[sensor]
+					m_, n_ = Q_.shape
+					Q = np.vstack([Q, np.zeros([m_,n])])
+					Q__ = np.vstack([np.zeros([m, n_]), Q_])
+					Q = np.hstack([Q, Q__])
+				else:
+					Q = self.Q_idx[sensor]
+			y = z - z_ 
+			S = H.dot(sigma_).dot(H.T) + Q 
+			K = sigma_.dot(H.T).dot(np.linalg.pinv(S))
+			self.mu = mu_ + (K.dot(y)).reshape([12,1])
+			self.sigma = (np.eye(12) - K.dot(H)).dot(sigma_)
+		else:
+			self.mu = mu_
+			self.sigma = sigma_
+		self.mu = mu_
+		self.sigma = sigma_ 
+
+	def step(self, U, dt, sensors=None, measurements=None):
+		mu_, sigma_ = self.predict(U, dt)
+		self.update(mu_, sigma_, U, sensors, measurements)
+		return self.mu, self.sigma 
--- dronestorm/estimate/sensors.py
+++ dronestorm/estimate/sensors.py
@@ -1,22 +1,21 @@
-import numpy as np
-from scipy.misc import derivative 
+import numpy as np 
 from state_matrix import * 
 ''' 
-X(0): x (global)
-X(1): y (global)
-X(2): z (global)
-X(3): phi
-X(4): theta
-X(5): psi
-X(6): xdot (global)
-X(7): ydot (global)
-X(8): zdot (global)
-X(9): wx
-X(10): wy
-X(11): wz
-'''
+        X(0): x (global)
+        X(1): y (global)
+        X(2): z (global)
+        X(3): phi
+        X(4): theta
+        X(5): psi
+        X(6): xdot (global)
+        X(7): ydot (global)
+        X(8): zdot (global)
+        X(9): wx
+        X(10): wy
+        X(11): wz
+        '''
 
-def IMU(X, U, X0=None):
+def IMU(X, U, Z0=None):
     '''
     Z[0] = xdd_body
     Z[1] = ydd_body
@@ -25,44 +24,61 @@ def IMU(X, U, X0=None):
     Z[4] = wy
     Z[5] = wz
     '''
-    Z = np.zeros([6,])
+    Z = np.zeros([9,])
 
     X_imu = np.array([0.01,0.01,0.01])
 
+    R_ = np.eye(3)
+    R_[0,0] = -1
+    R_[1,1] = -1
     R = rot(X[3],X[4],X[5])
-    Xd = x_dot(X, U)
 
+    Xd = x_dot(X, U)
     rot_accel = np.cross(X_imu, X[9:12].squeeze())
-    # import pdb; pdb.set_trace()
-    Z[0:3] = (R.dot(Xd[6:9]).squeeze() + rot_accel)
-    Z[3:6] = X[9:12].squeeze()
+    
+    Z[0:3] = (R_.dot(X[3:6])).squeeze()
+    Z[3:6] =  (R_.dot(R.dot(Xd[6:9]).squeeze() + rot_accel)).squeeze()
+    W = X[9:12].squeeze()
+    Z[6] = (W[0] + (W[1]*np.sin(X[3]) + W[2]*np.cos(X[3]))*np.tan(X[4])).squeeze()
+    Z[7] = (W[1]*np.cos(X[3]) - W[2]*np.sin(X[3])).squeeze()
+    Z[8] = ((W[1]*np.sin(X[3]) + W[2]*np.cos(X[3]))/np.cos(X[4])).squeeze()
     return Z 
 
-def AGL(X, U=None, X0=None):
+def AGL(X, U=None, Z0=None):
     z = X[2]/(np.cos(X[3])*np.cos(X[4]))
     return z 
 
-def GPS(X, U, X0):
+def GPS(X, U, Z0):
+    '''
+    Z[0] = Latitude
+    Z[1] = Longitude
+    Z[2] = Altitude
+    Z[3] = Heading
+    Z[4] = Speed
+    Z[5] = Climb
+    '''
+    X = X.squeeze()
     Z = np.zeros([6,])
-    Z[0:3] = (X[0:3] + X0[0:3]).squeeze()
-    Z[3] = np.linalg.norm(X[6:9])
-    Z[4] = np.arctan2(X[7],X[6])
+    Z[0] = Z0[0] + X[0]*np.cos(Z0[3]) - X[1]*np.sin(Z0[3])
+    Z[1] = Z0[1] + X[1]*np.cos(Z0[3]) + X[0]*np.sin(Z0[3])
+    Z[2] = Z0[2] + X[2]
+    Z[3] = Z0[3] + X[5]
+    Z[4] = np.linalg.norm(X[6:9])
     Z[5] = X[8]
     return Z 
 
 def gen_H(func, X, U, X0):
-    # H = np.zeros([0,12])
+    X = X.squeeze()
     for i in range(12):
         Xhi = X[:]*1.0
-        Xhi[i,0] = X[i,0] + 0.01
+        Xhi[i] = X[i] + 0.01
         Xlo = X[:]*1.0
-        Xlo[i,0] = X[i,0] - 0.01
+        Xlo[i] = X[i] - 0.01
 
         Zhi = func(Xhi, U, X0)
         Zlo = func(Xlo, U, X0)
-        # import pdb; pdb.set_trace()
         if 'H' in locals():
-            H = np.hstack([H,(Zhi - Zlo)/(2.0*0.01)])
+            H = np.hstack([H,((Zhi - Zlo)/(2.0*0.01)).reshape([-1,1])])
         else:
-            H = (Zhi - Zlo)/(2.0*0.01) 
-        return H 
+            H = ((Zhi - Zlo)/(2.0*0.01)).reshape([-1,1]) 
+    return H 
