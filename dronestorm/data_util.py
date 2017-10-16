b6bcbfbc8f8fec6a131a66d02824433f049da954
Sam Fok <sambfok@gmail.com>
2017-10-16 16:01:19 -0700

    wav file select from real and sim time

--- dronestorm/data_util.py
+++ dronestorm/data_util.py
@@ -9,7 +9,9 @@ AUDIO_SAMPLE_RATE = 44100 # standard 44.1kHz recording rate
 AUDIO_SAMPLE_DT = 1./AUDIO_SAMPLE_RATE
 
 def spike_wav(
-        fname_spikes, fname_wav, nrn_idx=None, fname_spike_kernel=None, dtype=np.int32,
+        fname_spikes, fname_wav, nrn_idx=None,
+        time_mode = "real",
+        fname_spike_kernel=None, wav_dtype=np.int32,
         plot=False):
     """Create a wav audio file from the spike data
 
@@ -24,25 +26,37 @@ def spike_wav(
     nrn_idx: list-like or none
         indices of neurons to use to generate wav file
         if None, uses all neurons, one per wav file channel
+    time_mode: "real" or "sim"
+        Whether to use the spike's real or simulation time
     fname_spike_kernel: string or None
         if not None, wav file name of kernel to convolve spikes with to generate different sounds
-    dtype: numpy dtype
+    wav_dtype: numpy dtype
         data type to be used in the wav file
     """
     assert isinstance(fname_spikes, str)
     assert isinstance(fname_wav, str)
+    assert time_mode in ["real", "sim"]
+
     file_data = np.loadtxt(fname_spikes)
-    time = file_data[:, 0]
-    spk_data_raw = file_data[:, 1:]
+    sim_time = file_data[:, 0]
+    measured_time = file_data[:, 1]
+    spk_data_raw = file_data[:, 2:]
     if nrn_idx is not None:
         spk_data_raw = spk_data_raw[:, nrn_idx]
     n_samples, n_neurons = spk_data_raw.shape
+
+    if time_mode == "real":
+        time = measured_time
+    elif time_mode == "sim":
+        time = sim_time
     n_resamples = int(np.ceil(time[-1]*AUDIO_SAMPLE_RATE))
     spk_data = np.zeros((n_resamples, n_neurons))
-    spk_idx = np.array(np.nonzero(spk_data_raw))
-    spk_idx[0, :] = np.clip(np.round(
-        spk_idx[0, :] * n_resamples / n_samples).astype(int), None, n_resamples)
-    spk_data[tuple(spk_idx)] = AUDIO_SAMPLE_RATE # impulse as 1/dt
+
+    for nrn_idx in range(n_neurons):
+        spk_idx = np.nonzero(spk_data_raw[:, nrn_idx])[0]
+        spk_times = time[spk_idx]
+        spk_resampled_idx = np.round(spk_times * AUDIO_SAMPLE_RATE).astype(int)
+        spk_data[spk_resampled_idx, nrn_idx] = AUDIO_SAMPLE_RATE # impulse as 1/dt
 
     if fname_spike_kernel is not None:
         assert isinstance(fname_spike_kernel, str)
@@ -61,8 +75,8 @@ def spike_wav(
         spk_data = spk_data_postconv
         spk_data = spk_data[shift_idx:] # align with original waveform
         spk_data = spk_data[:n_resamples] # clip to original length
-    np.clip(spk_data, np.iinfo(dtype).min, np.iinfo(dtype).max, spk_data)
-    spk_data = spk_data.astype(dtype)
+    np.clip(spk_data, np.iinfo(wav_dtype).min, np.iinfo(wav_dtype).max, spk_data)
+    spk_data = spk_data.astype(wav_dtype)
     wav_write(fname_wav, AUDIO_SAMPLE_RATE, spk_data)
 
     if plot:
